2e5945c271d962f30a9a8c2ea4a0fa752122cdad
Leonardo <leonardoandrade2003.197@gmail.com>
2024-10-30 08:20:03 -0500

    Agregar archivos de proyecto.

---
+++ LeonardoAndradeExamenProgresoOne_R/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using LeonardoAndradeExamenProgresoOne_R.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LeonardoAndradeExamenProgresoOne_R.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LeonardoAndradeExamenProgresoOne_R/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeonardoAndradeExamenProgresoOne_R.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
