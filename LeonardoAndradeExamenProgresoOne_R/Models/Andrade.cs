a832ddceceb913a7bee556c45c9d0b06bf62275b
Leonardo <leonardoandrade2003.197@gmail.com>
2024-10-30 08:42:57 -0500

    Clase usuario con mi nombre y clase celular realizada con todos los atributpos

---
+++ LeonardoAndradeExamenProgresoOne_R/Models/Andrade.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeonardoAndradeExamenProgresoOne_R.Models
+{
+    public class Andrade
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(20)]
+
+        public string name { get; set; }
+
+        [Range(0, 9000)]
+        public float sueldo { get; set; }
+
+        public Boolean empleo { get; set; }
+
+       
+        public DataType Cumple {  get; set; }
+    }
+}
---
+++ LeonardoAndradeExamenProgresoOne_R/Models/Celular.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeonardoAndradeExamenProgresoOne_R.Models
+{
+    public class Celular
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+        [MaxLength(20)]
+
+
+        public string Modelo { get; set; }
+        [Range(0, 300)]
+        public float precio  { get; set; }
+
+        [Range(0, 2023)]
+        public int año { get; set; }
+
+    }
+}
