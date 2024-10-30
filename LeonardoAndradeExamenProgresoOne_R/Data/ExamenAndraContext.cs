a1410c004eb678434be48d5ee94900d36a53c42e
Leonardo <leonardoandrade2003.197@gmail.com>
2024-10-30 09:04:21 -0500

    Context y migracion

---
+++ LeonardoAndradeExamenProgresoOne_R/Data/ExamenAndraContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LeonardoAndradeExamenProgresoOne_R.Data
+{
+    public class ExamenAndraContext : DbContext
+    {
+        public ExamenAndraContext(DbContextOptions<ExamenAndraContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeonardoAndradeExamenProgresoOne_R.Models.Andrade> Andrades { get; set; } = default!;
+        public DbSet<LeonardoAndradeExamenProgresoOne_R.Models.Celular> Celulars { get; set; } = default!;
+        
+    
+    }
+}
